d92d34b58164e840162f1319dd73c116b0937331
AbdillahdDie <43396110+didiberrypay@users.noreply.github.com>
2018-10-30 15:26:51 +0700

    second commit

--- src/main/java/com/berrypay/java/BerryApplication.java
+++ src/main/java/com/berrypay/java/BerryApplication.java
@@ -3,7 +3,7 @@ package com.berrypay.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//DIDI GANTENG SEKALI TAPI KALAH AMA ALDO DAN ALDO KALAH SAMA ANTON DAN ANTON KALAH SAMA IMAN
 @SpringBootApplication
 @EnableJpaAuditing
 public class BerryApplication {
