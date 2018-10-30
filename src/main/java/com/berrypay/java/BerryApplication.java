3775ba496fae15484c39ea55e04dfdc2d5a9bc4d
root <deni@berrypay.com>
2018-10-30 15:43:16 +0700

    third commit

--- src/main/java/com/berrypay/java/BerryApplication.java
+++ src/main/java/com/berrypay/java/BerryApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //DIDI GANTENG SEKALI TAPI KALAH AMA ALDO DAN ALDO KALAH SAMA ANTON DAN ANTON KALAH SAMA IMAN
+//DIDI BAU SEKALI
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class BerryApplication {
